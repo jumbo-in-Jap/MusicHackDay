957632dc3a8c61b5f61e06b8a3a5c74b25df8c56
jumbo ken <subsubmailer@gmail.com>
2013-01-01 09:25:39 +0900

    Initial Commit

---
+++ MusicHackDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicHackDay
+//
+//  Created by 羽田 健太郎 on 2013/01/01.
+//  Copyright (c) 2013年 羽田 健太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
